abb2ec091777bcbea9db868d2c738aa1be82a900
Heiko Engel <heikoengel@users.noreply.github.com>
2022-12-06 17:16:38 +0100

    day6

---
+++ day06.py
@@ -0,0 +1,12 @@
+with open("data06") as f:
+    data = f.readline()
+    if data.endswith("\n"):
+        data = data[:-1]
+
+def get_marker_pos(num_chars):
+    for i in range(0, len(data)-num_chars):
+        if len(set(data[i:i+num_chars])) == num_chars:
+            return (i+num_chars)
+
+print(get_marker_pos(4))
+print(get_marker_pos(14))
